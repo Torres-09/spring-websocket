8c255b944bdb3d87e72ba5632ccd8f3cd04d7270
hwan2da <dla612@naver.com>
2023-07-18 14:57:20 +0900

    feat: RabbitMQ Connect
    MessageBroker

--- src/main/java/com/hwan/websocketproject/configuration/WebSocketMessageBrokerConfiguration.java
+++ src/main/java/com/hwan/websocketproject/configuration/WebSocketMessageBrokerConfiguration.java
@@ -18,7 +18,12 @@ public class WebSocketMessageBrokerConfiguration implements WebSocketMessageBrok
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");
-        registry.setApplicationDestinationPrefixes("/pub");
+        registry.setApplicationDestinationPrefixes("/pub")
+                .enableStompBrokerRelay("/topic")
+                .setRelayHost("localhost")
+                .setVirtualHost("/")
+                .setRelayPort(61613)
+                .setClientLogin("guest")
+                .setClientPasscode("guest");
     }
 }
--- src/main/java/com/hwan/websocketproject/controller/MessageController.java
+++ src/main/java/com/hwan/websocketproject/controller/MessageController.java
@@ -3,6 +3,7 @@ package com.hwan.websocketproject.controller;
 import com.hwan.websocketproject.model.Message;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
@@ -12,9 +13,8 @@ public class MessageController {
     private final SimpMessageSendingOperations simpMessageSendingOperations;
 
     @MessageMapping("/hello")
-    public void message(Message message) {
-
-
-        simpMessageSendingOperations.convertAndSend("/sub/channel/" + message.getChannelId(), message);
+    public void message(Message message, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", message.getSender());
+        simpMessageSendingOperations.convertAndSend("/topic/" + message.getChannelId(), message);
     }
 }
